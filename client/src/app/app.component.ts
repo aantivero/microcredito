f0b8d0deb5f05311c2da25b6505373d2c369b195
Alejandro Antivero <aantivero@cajval.sba.com.ar>
2017-12-27 17:01:03 -0300

    Seguridad con Okta

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {OktaAuthService} from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Microcreditos';
+  constructor(private oktaAuth: OktaAuthService) {}
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MyOwnCustomMaterialModule } from './own.module';
 import { CuentaService } from './shared/cuenta/cuenta.service';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { CuentaListComponent } from './cuenta-list/cuenta-list.component';
 import { CuentaEditComponent } from './cuenta-edit/cuenta-edit.component';
-import { FormsModule} from '@angular/forms';
-import { RouterModule, Routes} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { OktaCallbackComponent, OktaAuthModule } from '@okta/okta-angular';
+import { AuthInterceptor } from './shared/okta/auth.interceptor';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: '/cuenta-list',
+    redirectTo: '/home',
     pathMatch: 'full'
   },
   {
@@ -29,14 +32,29 @@ const appRoutes: Routes = [
   {
     path: 'cuenta-edit/:id',
     component: CuentaEditComponent
+  },
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
+
+const config = {
+  issuer: 'https://dev-472261.oktapreview.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oadf3rz2xNcT5AI10h7'
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     CuentaListComponent,
-    CuentaEditComponent
+    CuentaEditComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -44,9 +62,10 @@ const appRoutes: Routes = [
     MyOwnCustomMaterialModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    OktaAuthModule.initAuth(config)
   ],
-  providers: [CuentaService],
+  providers: [CuentaService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
